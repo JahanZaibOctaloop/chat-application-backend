842f9fe5274d49197fa9dd620c5a2b62f4bb2e8e
JahanZaibOctaloop <jahanzaibasif898@gmail.com>
2024-07-18 17:01:41 +0500

    upgrade

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cors(corsOptions));
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'https://chat-application-azure-three.vercel.app'], // Replace with your frontend URLs
+        origin: 'https://chat-application-azure-three.vercel.app/',
         methods: ['GET', 'POST'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
